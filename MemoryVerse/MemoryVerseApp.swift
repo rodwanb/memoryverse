a8c711fc83d4881f6e994f350d92bd2df0e436e1
Rodwan Barbier <rodwanb@gmail.com>
2023-02-12 20:41:40 +0200

    initial commit

---
+++ MemoryVerse/MemoryVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryVerseApp.swift
+//  MemoryVerse
+//
+//  Created by Rodwan Barbier on 2023/02/12.
+//
+
+import SwiftUI
+
+@main
+struct MemoryVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
